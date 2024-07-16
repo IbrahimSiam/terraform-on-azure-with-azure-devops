5dd20f2d2707e38216a58a073ff7aa3bbe87ef70
Ibrahim Siam <ebrahemsiam@gmail.com>
2024-07-16 21:26:35 +0400

    CICD-Test-1

--- terraform-manifests/c7-05-web-linuxvm-resource.tf
+++ terraform-manifests/c7-05-web-linuxvm-resource.tf
@@ -9,7 +9,7 @@ sudo systemctl start httpd
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 sudo chmod -R 777 /var/www/html 
-sudo echo "Welcome to stacksimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
+sudo echo "This is Test Application For Azure DevOps CI/CD - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
 sudo mkdir /var/www/html/app1
 sudo echo "This is Test Application For Azure DevOps CI/CD - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/app1/hostname.html
 sudo echo "This is Test Application For Azure DevOps CI/CD - WebVM App1 - App Status Page" > /var/www/html/app1/status.html
